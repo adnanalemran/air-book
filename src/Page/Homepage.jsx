958709cab2ff401ae53ffe5b1a0183cb03a1d49c
adnanalemran <adnanalemranontor@gmail.com>
2024-03-19 18:02:27 +0600

    Subscribe  section complite

--- src/Page/Homepage.jsx
+++ src/Page/Homepage.jsx
@@ -4,6 +4,7 @@ import Featured from '../Componentes/Featured';
 import HeroSection from '../Componentes/HeroSection';
 import NavBar from '../Componentes/NavBar';
 import PopularAirlines from '../Componentes/PopularAirlines';
+import Subscribe from '../Componentes/Subscribe';
 import Testimonials from '../Componentes/Testimonials';
 import Travel from '../Componentes/Travel';
 
@@ -17,6 +18,7 @@ const Homepage = () => {
             <PopularAirlines/>
             <Testimonials/>
             <Travel/>
+            <Subscribe/>
         </div>
     );
 };
