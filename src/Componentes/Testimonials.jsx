5624ef51eef937f65e44fa2d9fca2bca47090cc7
adnanalemran <adnanalemranontor@gmail.com>
2024-03-19 19:13:11 +0600

    update content

--- src/Componentes/Testimonials.jsx
+++ src/Componentes/Testimonials.jsx
@@ -3,7 +3,7 @@ const Testimonials = () => {
     return (
         <div className="  ">
         <h2 className="py-8 container mx-auto px-8  text-2xl font-semibold text-gray-950">Popular Airlines</h2>
-          <div className="bg-blue-600 h-[300px]">Testimonials section </div>
+          <div className="bg-blue-600 h-[300px]">Testimonials section come in soon</div>
         </div>
     );
 };
